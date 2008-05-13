e246fef0ca0a70d661d047b3c1518c3b05e5a234
Jorge Bernal <jbernal@warp.es>
2008-05-13 22:12:13 +0200

    first commit

---
+++ publish.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ "x$2" == "x" ]; then
+    echo "Usage: $0 plugin_name version"
+    exit 1
+fi
+
+PLUGIN=$1
+VERSION=$2
+
+if [ ! -d "wp-content" ]; then
+    echo "You need to run this script from the wordpress base directory"
+    exit 2
+fi
+
+pushd wp-content/plugins/$PLUGIN
+perl -pe "s/Version: .*/Version: $VERSION/" -i $PLUGIN.php
+perl -pe "s/^define\(${PLUGIN}_version,.*/define(${PLUGIN}_version, '$VERSION');/" -i $PLUGIN.php
+
+git add $PLUGIN.php
+echo "Diff to commit"
+git diff --cached
+echo "Press return to accept or Ctrl-C to cancel"
+read foo
+git commit -m "Version bump to $VERSION"
+git tag -s v$VERSION -m "Version $VERSION"
+git push --mirror
+git archive --format=tar --prefix=$PLUGIN/ v$VERSION | gzip -c9 > $PLUGIN-$VERSION.tar.gz
+tar tzvf $PLUGIN-$VERSION.tar.gz
+scp $PLUGIN-$VERSION.tar.gz moe.warp.es:public_html/wp-plugins/$PLUGIN/
+popd
